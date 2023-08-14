b463d3f91629a52ac49ff28845c2b3108a8b67c7
unknown <rciupercher@gmail.com>
2023-08-14 16:43:53 +0200

    phone menu done

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 let menu = document.getElementById('menu_container')
 
 function openMenuFunction(){
+    menu.style.display = "block"
     menu.style.transform = "translateX(0%)"
+}
+
+function closePhoneMenu(){
+    menu.style.transform = "translateX(100%)"
+    menu.style.display = "none"
 }
